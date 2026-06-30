f53e653a18708d98c1a0814ff8b7d223e87cb692
LogicalGoy <ukmama22@gmail.com>
2026-06-30 21:47:05 +0100

    Update SametLinoriaShim.lua

--- SametLinoriaShim.lua
+++ SametLinoriaShim.lua
@@ -312,7 +312,7 @@ function Shim:Init(config)
 
         local W = { _win = win }
         local _tabIcons = {
-            Main = "123944728972740", ["Auto Parry"] = "80032814326075", Character = "134236649319095",
+            Main = "123944728972740", ["Auto Parry"] = "139844426124998", Character = "134236649319095",
             Aimbot = "100050851789190", ESP = "123554105934637", Utilities = "103180437044643",
             Settings = "126497581491926",
         }
